43a4c06d707cd174ee29f5552d5905bddeb0ee98
baybars <cem.baybars@gmail.com>
2019-06-18 23:59:53 +0300

    :arrow_up: upgrade the stand module by Official Stand v0.2.

--- t_system/stand/__init__.py
+++ t_system/stand/__init__.py
@@ -33,7 +33,7 @@ class Button:
         self.gpio_pin = gpio_pin
 
         self.is_pressed = False
-        self.is_press_long = False
+        self.pressed_hold_time = 0
         self.press_count = 0
 
         GPIO.setmode(GPIO.BCM)
@@ -45,13 +45,11 @@ class Button:
         """The low-level method to provide callback event to catching button's press state.
         """
         start_time = time.time()
-        # hold_time = start_time
 
         while GPIO.input(channel) == 0:  # Wait for the button up
-            hold_time = time.time() - start_time  # How long was the button down?
-            if hold_time >= 3:
-                self.is_press_long = True
-                break
+            self.pressed_hold_time = time.time() - start_time  # How long was the button down?
+            time.sleep(0.1)
+            pass
 
         # if hold_time >= .1:  # ignore noise
         #     self.is_pressed = True
@@ -74,6 +72,11 @@ class Button:
         """The top-level method to provide resetting of button's press count.
         """
         self.press_count = 0
+
+    def reset_pressed_hold_time(self):
+        """The top-level method to provide resetting of button's pressed hold time.
+        """
+        self.pressed_hold_time = 0
 
     def gpio_cleanup(self):
         """The low-level method to provide clean the GPIO pin that is reserved the button.
@@ -133,10 +136,7 @@ class Stand:
 
         self.vision = vision
 
-        self.standby_btn = Button(5)
-        self.augmented_btn = Button(6)
-        self.learn_btn = Button(13)
-        self.track_btn = Button(19)
+        self.multi_func_btn = Button(5)
 
         self.red_led = Led(27)
         self.green_led = Led(22)
@@ -151,21 +151,22 @@ class Stand:
 
         while True:
             print("running")
-            if self.standby_btn.is_press_long:
+            if self.multi_func_btn.pressed_hold_time >= 3:
                 self.release_members()
                 call("sudo poweroff", shell=True)
                 break  # this line maybe not necessary!!
 
-            elif self.standby_btn.is_pressed and self.standby_btn.press_count == 0:
+            elif self.multi_func_btn.pressed_hold_time >= 1.5:
                 print("Stand By")
-                self.reset_buttons(self.standby_btn, [self.augmented_btn, self.learn_btn, self.track_btn])
+                self.multi_func_btn.reset_pressed_hold_time()
+                self.reset_button()
                 self.terminate_threads(working_threads)
 
                 self.start_thread(self.blink_led, (lambda: self.stop_thread, self.red_led), working_threads)
 
-            elif self.augmented_btn.is_pressed and self.augmented_btn.press_count == 0:
+            elif self.multi_func_btn.is_pressed and self.multi_func_btn.press_count == 3:
                 print("Augmented active")
-                self.reset_buttons(self.augmented_btn, [self.standby_btn, self.learn_btn, self.track_btn])
+                self.reset_button()
                 self.terminate_threads(working_threads)
 
                 from t_system.augmented import Augmenter
@@ -174,17 +175,17 @@ class Stand:
                 self.start_thread(augmenter.run, (lambda: self.stop_thread,), working_threads)
                 self.start_thread(self.blink_led, (lambda: self.stop_thread, self.green_led, [1, 30]), working_threads)
 
-            elif self.learn_btn.is_pressed and self.learn_btn.press_count == 0:
+            elif self.multi_func_btn.is_pressed and self.multi_func_btn.press_count == 2:
                 print("Learn active")
-                self.reset_buttons(self.learn_btn, [self.standby_btn, self.augmented_btn, self.track_btn])
+                self.reset_button()
                 self.terminate_threads(working_threads)
 
                 self.start_thread(self.vision.learn, (lambda: self.stop_thread,), working_threads)
                 self.start_thread(self.blink_led, (lambda: self.stop_thread, self.green_led, [5, 5]), working_threads)
 
-            elif self.track_btn.is_pressed and self.track_btn.press_count == 0:
+            elif self.multi_func_btn.is_pressed and self.multi_func_btn.press_count == 1:
                 print("Track active")
-                self.reset_buttons(self.track_btn, [self.standby_btn, self.augmented_btn, self.learn_btn])
+                self.reset_button()
                 self.terminate_threads(working_threads)
 
                 self.start_thread(self.vision.detect_track, (lambda: self.stop_thread,), working_threads)
@@ -219,19 +220,13 @@ class Stand:
         self.red_led.off()
         self.green_led.off()
 
-    @staticmethod
-    def reset_buttons(this_button, other_buttons):
+    def reset_button(self):
         """The low-level method to releasing button states. Reset press count for other buttons and reset pressed state for currently pressed button.
-
-        Args:
-            this_button:       	        Button object that has been pressed.
-            other_buttons (list):       The list of Button objects those are to be wanted to reset.
         """
 
-        this_button.reset_press_state()
-
-        for button in other_buttons:
-            button.reset_press_count()
+        self.multi_func_btn.reset_press_state()
+        self.multi_func_btn.reset_press_count()
+        self.multi_func_btn.reset_pressed_hold_time()
 
     @staticmethod
     def start_thread(job, job_args, working_threads):
@@ -264,13 +259,10 @@ class Stand:
         """The low-level method to releasing camera, stop sending signals and clean up the gpio pins.
         """
 
-        self.vision.camera.release()
+        self.vision.release_camera()
         self.vision.release_servos()
 
-        self.standby_btn.gpio_cleanup()
-        self.augmented_btn.gpio_cleanup()
-        self.learn_btn.gpio_cleanup()
-        self.track_btn.gpio_cleanup()
+        self.multi_func_btn.gpio_cleanup()
 
         self.red_led.gpio_cleanup()
         self.green_led.gpio_cleanup()
