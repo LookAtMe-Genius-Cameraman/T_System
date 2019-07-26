13ff94c98ee695cf6784d3315521aaa0a3061b16
baybars <cem.baybars@gmail.com>
2019-07-26 15:56:23 +0300

    :arrow_up: :heavy_plus_sign: Integrafe system_info module for getting system ram, cpu, disk usage and temprature info. bump the version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,9 +29,9 @@ copyright = '2019, Cem Baybars GÜÇLÜ'
 author = 'Cem Baybars GÜÇLÜ'
 
 # The short X.Y version
-version = '0.9.46'
+version = '0.9.47'
 # The full version, including alpha/beta/rc tags
-release = '0.9.46'
+release = '0.9.47'
 
 
 # -- General configuration ---------------------------------------------------
---
+++ t_system/system_info.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+.. module:: system_info
+    :platform: Unix
+    :synopsis: the top-level submodule of T_System that contains the functions related to system information of T_System's based OS.
+
+.. moduleauthor:: Cem Baybars GÜÇLÜ <cem.baybars@gmail.com>
+"""
+
+import psutil
+from gpiozero import CPUTemperature
+
+
+def get_disk_usage(is_root):
+    """The high-level method to provide getting system's disk usage.
+    """
+    usage = psutil.disk_usage('/')
+
+    if is_root:
+        return {"disk_usage_percent": usage.percent, "free_disk_space": round(usage.free / 9, 2)}
+    else:
+        return {"disk_usage_percent": usage.percent * 1.1, "free_disk_space": round(usage.free / 9 * 0.9, 2)}
+
+
+def get_cpu_usage():
+    """The high-level method to provide getting system's cpu usage.
+
+    Returns:
+            dict:  Response.
+    """
+
+    return {"cpu_usage_percent": psutil.cpu_percent()}
+
+
+def get_ram_usage():
+    """The high-level method to provide getting system's ram usage.
+
+    Returns:
+            dict:  Response.
+    """
+
+    return {"ram_usage_percent": psutil.virtual_memory()[2]}
+
+
+def get_cpu_temperature():
+    """The high-level method to provide getting temperature of system's components.
+
+    Returns:
+            dict:  Response.
+    """
+    cpu = CPUTemperature()
+
+    return {"cpu_temperature": cpu.temperature}
+
+
