040030ca39f8528202f9d0ea04ec3eaac0b84521
baybars <cem.baybars@gmail.com>
2019-08-01 09:34:22 +0300

    :arrow_up: Bump the version t_system 0.9-alpha1.65 -> 0.9-alpha1.66 remote_ui 0.0.9 -> 0.1.0

--- t_system/__init__.py
+++ t_system/__init__.py
@@ -21,7 +21,7 @@ from t_system.vision import Vision
 from t_system.accession import AccessPoint
 from t_system.administration import Administrator
 
-__version__ = '0.9-alpha1.65'
+__version__ = '0.9-alpha1.66'
 
 T_SYSTEM_PATH = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 
