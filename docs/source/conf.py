fb214e3840668ccff761b209842b7373e6a01470
baybars <cem.baybars@gmail.com>
2019-08-05 11:17:18 +0300

    :arrow_up: Bump the version t_system 0.9-alpha1.75 -> 0.9-alpha1.76

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,7 @@ author = 'Cem Baybars GÜÇLÜ'
 # The short X.Y version
 version = '0.9'
 # The full version, including alpha/beta/rc tags
-release = '0.9-alpha1.75'
+release = '0.9-alpha1.76'
 
 
 # -- General configuration ---------------------------------------------------
