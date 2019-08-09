95bf0276953a1161a42b1bf6efcda66211ebeb41
baybars <cem.baybars@gmail.com>
2019-08-09 22:33:15 +0300

    :heavy_plus_sign: Integrate a database fetcher to provide to methods own db tables

---
+++ t_system/db_fetching.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+.. module:: database
+    :platform: Unix
+    :synopsis: the top-level submodule of T_System that contains the functions and classes related to T_System's database and table creation ability. Powered by TinyDB.
+
+.. moduleauthor:: Cem Baybars GÜÇLÜ <cem.baybars@gmail.com>
+"""
+
+from tinydb import TinyDB, Query  # TinyDB is a lightweight document oriented database
+
+
+class DBFetcher:
+    """Class to define an database handler with given folder and table name. It's only job creating databases or tables and returning them.
+
+    This class provides necessary initiations and functions named :func:`t_system.database.Database.get`
+    for return TinyDB database object.
+    """
+
+    def __init__(self, folder, name, table=None, cache_size=None):
+        """Initialization method of :class:`t_system.database.Database` class.
+
+        Args:
+            folder (str)                    Folder that contains database.
+            name (str)                      Name of the database.
+            table (str):                    Current working table name.
+            cache_size (int):               TinyDB caches query result for performance.
+        """
+
+        self.folder = folder
+        self.name = name
+        self.table = table
+        self.cache_size = cache_size
+
+    def fetch(self):
+        """The high-level method to return database. If there is a table name method  creates a table and returns that. Otherwise returns all db.
+
+        Returns:
+                TinyDB: database object.
+        """
+
+        db = TinyDB(f'{self.folder}/{self.name}.json')
+
+        if self.table:
+            return self.set_table(db, self.table, self.cache_size)
+
+        return db
+
+    @staticmethod
+    def set_table(db, table_name, cache_size=None):
+        """The low-level method to set database by table name.
+
+        Args:
+            db (TinyDB):                    TinyDB object.
+            table_name (str):               Current working table name.
+            cache_size (int):               TinyDB caches query result for performance.
+        """
+
+        return db.table(table_name, cache_size=cache_size)
