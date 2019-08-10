fbb34a9fdee2f5fb15361546dd14f160c558fbfe
baybars <cem.baybars@gmail.com>
2019-08-10 22:46:31 +0300

    :heavy_plus_sign: Add 'remote_ui' an handle point to starting self-update and setting auto-update

---
+++ t_system/remote_ui/api/update.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+.. module:: update
+    :platform: Unix
+    :synopsis: the top-level submodule of T_System's remote_ui that contains the API for T_System's self update ability.
+
+.. moduleauthor:: Cem Baybars GÜÇLÜ <cem.baybars@gmail.com>
+"""
+
+from flask import Blueprint, request
+from flask_restful import Api, Resource
+from schema import SchemaError
+
+from t_system.remote_ui.modules.update import create_position, get_position, get_status, update_status, delete_position
+from t_system.remote_ui.api.data_schema import UPDATE_SCHEMA
+
+api_bp = Blueprint('update_api', __name__)
+
+api = Api(api_bp)
+
+
+class UpdateApi(Resource):
+    """Class to define an API of the positions of the arm.
+
+        This class provides necessary initiations and functions named;
+         :func:`t_system.remote_ui.api.update.UpdateApi.get`for the provide get auto-update status and start update with getting finished status,
+         :func:`t_system.remote_ui.api.update.UpdateApi.post` INVALID,
+         :func:`t_system.remote_uia.api.update.UpdateApi.put` for provide updating the auto-update status,
+         :func:`t_system.remote_ui.api.update.UpdateApi.delete` INVALID
+    """
+
+    def __init__(self):
+        """Initialization method of :class:`t_system.remote_ui.api.update.UpdateApi` class.
+        """
+
+    def get(self):
+        """The API method to get request for flask.
+        """
+
+        update_key = request.args.get('key')
+        admin_id = request.args.get('admin_id', None)
+
+        if not update_key:
+            return {'status': 'ERROR', 'message': '\'id\' parameter is missing'}
+
+        status = get_status(admin_id, update_key)
+
+        return {'status': 'OK', 'data': status}
+
+    def post(self):
+        """The API method to post request for flask.
+        """
+
+        return {'status': 'ERROR', 'message': 'NOT VALID'}
+
+    def put(self):
+        """The API method to put request for flask.
+        """
+        admin_id = request.args.get('admin_id', None)
+
+        data = UPDATE_SCHEMA.validate(request.form)
+
+        result = update_status(admin_id, data)
+        return {'status': 'OK' if result else 'ERROR'}
+
+    def delete(self):
+        """The API method to delete request for flask.
+        """
+
+        return {'status': 'ERROR', 'message': 'NOT VALID'}
+
+
+api.add_resource(UpdateApi, '/api/update')
---
+++ t_system/remote_ui/modules/update.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+.. module:: update
+    :platform: Unix
+    :synopsis: the top-level submodule of T_System's remote_ui that contains the functions for managing of t_system's auto-update.
+
+.. moduleauthor:: Cem Baybars GÜÇLÜ <cem.baybars@gmail.com>
+"""
+
+from t_system import update_manager
+
+
+def get_status(admin_id, update_key):
+    """The high-level method to return existing positions.
+
+    Args:
+        admin_id (str):                 Root privileges flag.
+        update_key (str):               Key of the requested status.
+    """
+
+    if update_key == "auto_update":
+        status = update_manager.is_update_auto()
+    elif update_key == "up_to_date":
+        status = update_manager.update()
+    else:
+        status = None
+
+    return status
+
+
+def update_status(admin_id, data):
+    """The high-level method to update the position that is recorded in database with given parameters.
+
+    Args:
+        admin_id (str):                 Root privileges flag.
+        data (dict):                    Update data structure.
+    """
+    update_manager.change_members(data["auto_update"])
+    result = True
+    return result
