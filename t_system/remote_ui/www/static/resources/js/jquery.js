8f6b8e6e59d9483f8b13b854ffc51553fbab2b84
baybars <cem.baybars@gmail.com>
2019-08-16 00:12:27 +0300

    :art: Move the jquery code blocks under 'jquery.js' file :memo: Add the comments to methods thoose are inside 'jquery.js'.

---
+++ t_system/remote_ui/www/static/resources/js/jquery.js
@@ -0,0 +1,140 @@
+// -*- coding: utf-8 -*-
+
+/**
+ * @module jquery
+ * @fileoverview the top-level module of T_System that contains the jquery request classes and methods to flask of T_System.
+ * @author cem.baybars@gmail.com (Cem Baybars GÜÇLÜ)
+ */
+
+let requested_data = undefined;
+let response_data = undefined;
+
+/**
+ * The high-level method to handle of PUT request of $.ajax query.
+ * @param {string} url: the route address of the flask
+ * @param data: posting data to route.
+ * @param {function} callback: the callback function.
+ * @return {ajax} Whether something occurred.
+ */
+$.put = function(url, data, callback){
+
+  if ( $.isFunction(data) ){
+    callback = data;
+    data = {}
+  }
+
+  return $.ajax({
+    url: url,
+    type: 'PUT',
+    success: callback,
+    data: data,
+    contentType: 'application/json'
+  });
+};
+
+/**
+ * The high-level method to handle of DELETE request of $.ajax query.
+ * @param {string} url: the route address of the flask
+ * @param data: posting data to route.
+ * @param {function} callback: the callback function.
+ * @return {ajax} Whether something occurred.
+ */
+$.delete = function(url, data, callback){
+
+  if ( $.isFunction(data) ){
+        callback = data;
+        data = {}
+  }
+
+  return $.ajax({
+    url: url,
+    type: 'DELETE',
+    success: callback,
+    data: data,
+    contentType: 'application/json'
+  });
+};
+
+/**
+ * Class to define a AJAX JQuery communication method manager object.
+ */
+class JQueryManager {
+
+    constructor() {
+    }
+
+    /**
+     * The high-level method to sending data that is given with data parameter to the route.
+     * @param {string} route: the route address of the flask
+     * @param {dict} data: posting data to route.
+     * @return {string} Whether something occurred.
+     */
+    static post_data(route, data) {
+
+        let success = "";
+        // console.log(data);
+        $.post(route, data, function (req, err, resp) {
+            response_data = JSON.parse(resp.responseText);
+            // console.log(response_datavfdv);
+
+            success = err
+        });
+        // stop link reloading the page
+        // event.preventDefault();
+
+        return success
+    }
+
+    /**
+     * The high-level method to getting data from given route as response.
+     * The getting dictionary data from python flask assigns to `requested_data` variable.
+     * @param {string} route: the route address of the flask
+     */
+    static get_data(route) {
+        let success = "";
+
+        $.get(route, function (data, err) {
+            success = err;
+
+            if (success === "success") {
+                requested_data = data;
+            }
+        });
+    }
+
+    /**
+     * The high-level method to send DELETE request to given route.
+     * The response of request assigns to `response_data` variable.
+     * @param {string} route: the route address of the flask
+     */
+    static delete_data(route) {
+        let success = "";
+
+        $.delete(route, function (req, err, resp) {
+
+            success = err;
+            if (success === "success") {
+                response_data = JSON.parse(resp.responseText);
+                success = err
+            }
+        });
+    }
+
+    /**
+     * The high-level method to send PUT request to given route.
+     * The response of request assigns to `response_data` variable.
+     * @param {string} route: the route address of the flask
+     * @param {dict} data: putting data to route.
+     */
+    static put_data(route, data) {
+        let success = "";
+        $.put(route, data, function (req, err, resp) {
+            response_data = JSON.parse(resp.responseText);
+            success = err
+        });
+        // stop link reloading the page
+        // event.preventDefault();
+
+        return success
+    }
+}
--- t_system/remote_ui/www/static/resources/js/main.js
+++ t_system/remote_ui/www/static/resources/js/main.js
@@ -4,155 +4,51 @@
  * @module main
  * @fileoverview the top-level module of T_System that contains the communication methods with python flask of T_System.
  * @author cem.baybars@gmail.com (Cem Baybars GÜÇLÜ)
- * @version 0.01
  */
 
 let admin_id = false;
 let timer;
-let requested_data = undefined;
-let response_data = undefined;
-
-$.put = function(url, data, callback, type){
-
-  if ( $.isFunction(data) ){
-    type = type || callback;
-    callback = data;
-    data = {}
-  }
-
-  return $.ajax({
-    url: url,
-    type: 'PUT',
-    success: callback,
-    data: data,
-    contentType: type
-  });
-};
-
-$.delete = function(url, data, callback, type){
-
-  if ( $.isFunction(data) ){
-    type = type || callback,
-        callback = data,
-        data = {}
-  }
-
-  return $.ajax({
-    url: url,
-    type: 'DELETE',
-    success: callback,
-    data: data,
-    contentType: type
-  });
-};
-
-/**
- * Class to define a AJAX JQuery communication method manager object.
- */
-class JQueryManager {
-
-    constructor() {
-    }
-
-    /**
-     * The high-level method to sending data that is given with data parameter to the route.
-     * @param {string} route: the route address of the flask
-     * @param {dict} data
-     * @return {string} Whether something occurred.
-     */
-    static post_data(route, data) {
-
-        let success = "";
-        // console.log(data);
-        $.post(route, data, function (req, err, resp) {
-            response_data = JSON.parse(resp.responseText);
-            // console.log(response_datavfdv);
-
-            success = err
-        });
-        // stop link reloading the page
-        // event.preventDefault();
-
-        return success
-    }
-
-    /**
-     * The high-level method to getting data from given route as response.
-     * @param {string} route: the route address of the flask
-     * @return {requested_data} Getting dictionary data from python flask.
-     */
-    static get_data(route) {
-        let success = "";
-
-        $.get(route, function (data, err) {
-
-            success = err;
-            // console.log(data);
-            // console.log(err);
-            if (success === "success") {
-                requested_data = data;
-                // console.log(requested_data);
-                if (timer !== undefined){
-                    clearInterval(timer);
-                }
-            }
-        });
-    }
-
-    static delete_data(route) {
-        let success = "";
-
-        $.delete(route, function (req, err, resp) {
-
-            success = err;
-            // console.log(data);
-            // console.log(err);
-            if (success === "success") {
-                response_data = JSON.parse(resp.responseText);
-                // console.log(response_datavfdv);
-
-                success = err
-            }
-        });
-    }
-}
 
 /** @type {!Object} */
 const jquery_manager = JQueryManager;
 
+/** @type {!Element} */
 const create_new_event_div = document.getElementById("create_new_event");
 const controlling_template_container = document.getElementById("controlling_template_container");
 const settings_template_container = document.getElementById("settings_template_container");
 const on_work_template_container = document.getElementById("on_work_template_container");
 const managing_bar = document.getElementById("managing_bar");
 
-
+/** @type {!Element} */
 const control_btn = document.getElementById("control_btn");
 const settings_btn = document.getElementById("settings_btn");
 const on_work_btn = document.getElementById("on_work_btn");
 
-window.onload = function() {
+window.onload = function () {
     //to check if javascript is disabled like in anroid preview
     // document.getElementById('warningmsg').style.display = 'none';
     connectdlg();
 };
 
-function build_language_menu(){
+/**
+ * The low-level method to create drop-down language selection menu.
+ */
+function build_language_menu() {
     let content = "";
-    for (let lang_i =0 ; lang_i < language_list.length; lang_i++){
-    content+="<a href='#' onclick=\"translate_text('";
-    content+= language_list[lang_i][0];
-    content+= "'); update_ui_text();\"><span >";
-    content+= language_list[lang_i][1];
-    content+= "</span><span class=\"clearfix\"></span></a>";
-    if ( language_list[lang_i][0] === language){
-        document.getElementById("translate_menu").innerHTML=language_list[lang_i][1];
+    for (let lang_i = 0; lang_i < language_list.length; lang_i++) {
+        content += "<a href='#' onclick=\"translate_text('";
+        content += language_list[lang_i][0];
+        content += "'); update_ui_text();\"><span >";
+        content += language_list[lang_i][1];
+        content += "</span><span class=\"clearfix\"></span></a>";
+        if (language_list[lang_i][0] === language) {
+            document.getElementById("translate_menu").innerHTML = language_list[lang_i][1];
         }
-}
-document.getElementById("lang_menu").innerHTML=content;
+    }
+    document.getElementById("lang_menu").innerHTML = content;
 }
 
-function update_ui_text(){
+function update_ui_text() {
     build_HTML_setting_list(current_setting_filter);
 }
 
@@ -166,7 +62,7 @@ function hide_element(element) {
     element.style.visibility = "hidden";
 }
 
-function refresh_page(){
+function refresh_page() {
     window.location.reload();
 }
 
