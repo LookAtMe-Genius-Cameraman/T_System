e97f700aa2631a6d60e821c2e368bbb85cbc22de
baybars <cem.baybars@gmail.com>
2019-08-19 08:22:29 +0300

    Add 'dbus' libraries to installer scripts

--- install-dev.sh
+++ install-dev.sh
@@ -6,8 +6,8 @@ eval set -- "${OPTS}"
 apt-get update
 apt-get -y install debhelper python3 python3-all-dev libglib2.0-dev libcairo2-dev libgtk2.0-dev && \
 apt-get -y install dpkg python3-minimal ${misc:Pre-Depends} && \
-apt-get -y install ${python3:Depends} ${misc:Depends} flite python3-xlib portaudio19-dev python3-all-dev flac libnotify-bin python3-lxml python3-pyaudio python3-httplib2 python3-pip python3-setuptools python3-wheel libgstreamer1.0-dev gstreamer1.0-plugins-good gstreamer1.0-tools subversion libatlas-base-dev automake autoconf libtool libgtk2.0-0 gir1.2-gtk-3.0 && \
-apt-get -y install dnsmasq hostapd
+apt-get -y install ${python3:Depends} ${misc:Depends} flite python3-xlib portaudio19-dev python3-all-dev flac libnotify-bin python3-lxml python3-pyaudio python3-httplib2 python3-pip python3-setuptools python3-wheel libgstreamer1.0-dev gstreamer1.0-plugins-good gstreamer1.0-tools subversion libatlas-base-dev automake autoconf libtool libgtk2.0-0 gir1.2-gtk-3.0 dbus libdbus-glib-1-dev libdbus-1-dev python-dbus && \
+t_  apt-get -y install dnsmasq hostapd
 
 pip3 install -e .
 
