1a24ac11d03a61dbc11a0737ccb0fb349455b99e
baybars <cem.baybars@gmail.com>
2019-08-19 16:30:36 +0300

    :arrow_up: Bump the version t_system 0.9-alpha2.11 -> 0.9-alpha2.12 remote_ui 0.3 -> 0.3.1

--- t_system/__init__.py
+++ t_system/__init__.py
@@ -33,7 +33,7 @@ from t_system.accession import AccessPoint
 from t_system.administration import Administrator
 from t_system.updation import UpdateManager
 
-__version__ = '0.9-alpha2.11'
+__version__ = '0.9-alpha2.12'
 
 
 def start(args):
