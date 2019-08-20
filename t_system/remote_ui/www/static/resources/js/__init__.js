e01086a370ad456a0443b45b08fb7d1be16c16b9
baybars <cem.baybars@gmail.com>
2019-08-20 12:50:52 +0300

    :heavy_plus_sign: Add '__init__.js' file to declaring global variables.

---
+++ t_system/remote_ui/www/static/resources/js/__init__.js
@@ -0,0 +1,13 @@
+// -*- coding: utf-8 -*-
+
+/**
+ * @module main
+ * @fileoverview the top-level module of T_System that contains the communication methods with python flask of T_System.
+ * @author cem.baybars@gmail.com (Cem Baybars GÜÇLÜ)
+ */
+
+let admin_id = false;
+let timer;
+
+let requested_data = null;
+let response_data = null;
