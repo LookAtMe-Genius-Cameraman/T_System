271ac8afa81a0c0e6678e63b8968c9fd60d95c5e
baybars <cem.baybars@gmail.com>
2019-08-26 12:40:18 +0300

    :newspaper: Integrate 'jsalert.js' to handling 'sweetalert.js'

---
+++ t_system/remote_ui/www/static/resources/js/jsalert.js
@@ -0,0 +1,41 @@
+// -*- coding: utf-8 -*-
+
+/**
+ * @module jsalert
+ * @fileoverview the top-level module of T_System that contains the custom alert method of remote_ui of T_System.
+ * @author cem.baybars@gmail.com (Cem Baybars GÜÇLÜ)
+ */
+
+
+function JSalert(title, text, confirm_btn_text, cancel_btn_text, confirm_function) {
+    swal({
+            title: title,
+            text: text,
+            icon: "warning",
+            buttons: {
+                cancel: {
+                    text: cancel_btn_text,
+                    value: null,
+                    visible: true,
+                    className: "",
+                    closeModal: true,
+                },
+                confirm: {
+                    text: confirm_btn_text,
+                    value: true,
+                    visible: true,
+                    className: "",
+                    closeModal: true
+                }
+            },
+            // confirmButtonColor: "#DD6B55",
+        }
+    ).then(function (isConfirm) {
+        if (isConfirm) {
+            confirm_function();
+            swal(translate_text_item("Process Completed!"), "", "success");
+        } else {
+            // swal("Hurray", "Account is not removed!", "error");
+        }
+    });
+}
