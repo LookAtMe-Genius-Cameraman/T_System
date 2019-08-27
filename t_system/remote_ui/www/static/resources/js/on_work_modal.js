0ec4b0b6146b19ca49e0eb553d17397890d3a45f
baybars <cem.baybars@gmail.com>
2019-08-27 15:17:25 +0300

    :art: Add Scenario and parameter specifying panels, Ready, Start and Stop status button to 'on_work_modal'.

--- t_system/remote_ui/www/static/resources/js/on_work_modal.js
+++ t_system/remote_ui/www/static/resources/js/on_work_modal.js
@@ -1,27 +1,70 @@
-const on_work_windows_size_select = document.getElementById("on_work_windows_size_select");
+const on_work_job_btn = document.getElementById("on_work_job_btn");
 
-/**
- * The high-level method to playing and pausing the streams from t_system.
- */
-function playPause() {
-  if (stream_area_img.paused)
-    stream_area_img.play();
-  else
-    stream_area_img.pause();
+const monitor_area_div = document.getElementById("monitor_area_div");
+
+const main_specify_div = document.getElementById("main_specify_div");
+
+const ai_select_checkbox = document.getElementById("ai_select_checkbox");
+const ai_select_cb_label = document.getElementById("ai_select_cb_label");
+const security_mode_checkbox = document.getElementById("security_mode_checkbox");
+const security_mode_cb_label = document.getElementById("security_mode_cb_label");
+const non_moving_target_checkbox = document.getElementById("non_moving_target_checkbox");
+const non_moving_target_cb_label = document.getElementById("non_moving_target_cb_label");
+const time_laps_checkbox = document.getElementById("time_laps_checkbox");
+const time_laps_cb_label = document.getElementById("time_laps_cb_label");
+
+function shine_checked_boxes(elements) {
+
+    let labels = document.getElementsByTagName('label');
+
+    for (let i = 0; i < elements.length; i++) {
+        if (elements[i].checked) {
+            for (let a = 0; a < labels.length; a++) {
+                if (labels[a].htmlFor === elements[i].id){
+                    labels[a].classList.toggle("shine_in_dark");
+                }
+            }
+        }
+    }
 }
 
-/**
- * The high-level method to set size of the video window by given parameter.
- * @param {int} size: size value of window.
- */
-function set_window_size(size) {
-        stream_area_img.width = size;
-}
 
-on_work_windows_size_select.addEventListener("change", function () {
+let on_work_job_btn_click_count = 0;
 
-    if (window_size_select.value === "small"){set_window_size(320)}
-    else if (window_size_select.value === "normal"){set_window_size(420)}
-    else if (window_size_select.value === "big"){set_window_size(560)}
 
+on_work_job_btn.addEventListener("click", function () {
+    on_work_job_btn_click_count++;
+        if(on_work_job_btn_click_count <= 1) {
+            on_work_job_btn.innerHTML = "START";
+            on_work_job_btn.classList.toggle("ready");
+            on_work_job_btn.classList.toggle("btn-warning");
+            on_work_job_btn.classList.toggle("btn-danger");
+
+            main_specify_div.classList.toggle("hidden_element_without_child");
+            // main_specify_div.style.visibility = "hidden";
+            dark_deep_background_div.classList.toggle("focused");
+
+            shine_checked_boxes([ai_select_checkbox, security_mode_checkbox, non_moving_target_checkbox, time_laps_checkbox])
+
+        } else if (on_work_job_btn_click_count <= 2) {
+            on_work_job_btn.innerHTML = "STOP";
+            on_work_job_btn.classList.toggle("ready");
+            on_work_job_btn.classList.toggle("btn-danger");
+            on_work_job_btn.classList.toggle("btn-dark");
+
+            monitor_area_div.classList.toggle("focused");
+
+            shine_checked_boxes([ai_select_checkbox, security_mode_checkbox, non_moving_target_checkbox, time_laps_checkbox])
+
+        } else {
+            on_work_job_btn.innerHTML = "READY";
+            on_work_job_btn.classList.toggle("btn-dark");
+            on_work_job_btn.classList.toggle("btn-warning");
+            dark_deep_background_div.classList.toggle("focused");
+
+            main_specify_div.classList.toggle("hidden_element_without_child");
+            monitor_area_div.classList.toggle("focused");
+
+            on_work_job_btn_click_count = 0;
+        }
 });
