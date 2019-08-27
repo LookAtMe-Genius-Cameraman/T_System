e9dac6bf00b09cc907e12fd9927d1de8f6a617da
baybars <cem.baybars@gmail.com>
2019-08-27 16:14:07 +0300

    :art: Integrate cancel button to 'on_work_modal'.

--- t_system/remote_ui/www/static/resources/js/on_work_modal.js
+++ t_system/remote_ui/www/static/resources/js/on_work_modal.js
@@ -1,4 +1,5 @@
 const on_work_job_btn = document.getElementById("on_work_job_btn");
+const on_work_cancel_btn = document.getElementById("on_work_cancel_btn");
 
 const monitor_area_div = document.getElementById("monitor_area_div");
 
@@ -20,7 +21,7 @@ function shine_checked_boxes(elements) {
     for (let i = 0; i < elements.length; i++) {
         if (elements[i].checked) {
             for (let a = 0; a < labels.length; a++) {
-                if (labels[a].htmlFor === elements[i].id){
+                if (labels[a].htmlFor === elements[i].id) {
                     labels[a].classList.toggle("shine_in_dark");
                 }
             }
@@ -34,37 +35,59 @@ let on_work_job_btn_click_count = 0;
 
 on_work_job_btn.addEventListener("click", function () {
     on_work_job_btn_click_count++;
-        if(on_work_job_btn_click_count <= 1) {
-            on_work_job_btn.innerHTML = "START";
-            on_work_job_btn.classList.toggle("ready");
-            on_work_job_btn.classList.toggle("btn-warning");
-            on_work_job_btn.classList.toggle("btn-danger");
+    if (on_work_job_btn_click_count <= 1) {
+        on_work_job_btn.innerHTML = "START";
+        on_work_job_btn.classList.toggle("ready");
+        on_work_job_btn.classList.toggle("btn-warning");
+        on_work_job_btn.classList.toggle("btn-danger");
 
-            main_specify_div.classList.toggle("hidden_element_without_child");
-            // main_specify_div.style.visibility = "hidden";
-            dark_deep_background_div.classList.toggle("focused");
+        on_work_cancel_btn.classList.toggle("active");
 
-            shine_checked_boxes([ai_select_checkbox, security_mode_checkbox, non_moving_target_checkbox, time_laps_checkbox])
+        main_specify_div.classList.toggle("hidden_element_without_child");
+        // main_specify_div.style.visibility = "hidden";
+        dark_deep_background_div.classList.toggle("focused");
 
-        } else if (on_work_job_btn_click_count <= 2) {
-            on_work_job_btn.innerHTML = "STOP";
-            on_work_job_btn.classList.toggle("ready");
-            on_work_job_btn.classList.toggle("btn-danger");
-            on_work_job_btn.classList.toggle("btn-dark");
+        shine_checked_boxes([ai_select_checkbox, security_mode_checkbox, non_moving_target_checkbox, time_laps_checkbox])
 
-            monitor_area_div.classList.toggle("focused");
+    } else if (on_work_job_btn_click_count <= 2) {
+        on_work_job_btn.innerHTML = "STOP";
+        on_work_job_btn.classList.toggle("ready");
+        on_work_job_btn.classList.toggle("btn-danger");
+        on_work_job_btn.classList.toggle("btn-dark");
 
-            shine_checked_boxes([ai_select_checkbox, security_mode_checkbox, non_moving_target_checkbox, time_laps_checkbox])
+        on_work_cancel_btn.classList.toggle("active");
 
-        } else {
-            on_work_job_btn.innerHTML = "READY";
-            on_work_job_btn.classList.toggle("btn-dark");
-            on_work_job_btn.classList.toggle("btn-warning");
-            dark_deep_background_div.classList.toggle("focused");
+        monitor_area_div.classList.toggle("focused");
 
-            main_specify_div.classList.toggle("hidden_element_without_child");
-            monitor_area_div.classList.toggle("focused");
+        shine_checked_boxes([ai_select_checkbox, security_mode_checkbox, non_moving_target_checkbox, time_laps_checkbox])
 
-            on_work_job_btn_click_count = 0;
-        }
+    } else {
+        on_work_job_btn.innerHTML = "READY";
+        on_work_job_btn.classList.toggle("btn-dark");
+        on_work_job_btn.classList.toggle("btn-warning");
+        dark_deep_background_div.classList.toggle("focused");
+
+        main_specify_div.classList.toggle("hidden_element_without_child");
+        monitor_area_div.classList.toggle("focused");
+
+        on_work_job_btn_click_count = 0;
+    }
+});
+
+
+on_work_cancel_btn.addEventListener("click", function () {
+    on_work_cancel_btn.classList.toggle("active");
+
+    on_work_job_btn.innerHTML = "READY";
+    on_work_job_btn.classList.toggle("ready");
+    on_work_job_btn.classList.toggle("btn-danger");
+    on_work_job_btn.classList.toggle("btn-warning");
+    dark_deep_background_div.classList.toggle("focused");
+
+    main_specify_div.classList.toggle("hidden_element_without_child");
+
+    shine_checked_boxes([ai_select_checkbox, security_mode_checkbox, non_moving_target_checkbox, time_laps_checkbox]);
+
+
+    on_work_job_btn_click_count = 0
 });
