2b476070bc5602067685303722284fac6d61ce44
baybars <cem.baybars@gmail.com>
2019-09-03 16:22:57 +0300

    :art: Integrate 'audio_input' option to the 'settings_modal'.

--- t_system/remote_ui/www/static/resources/js/settings_modal.js
+++ t_system/remote_ui/www/static/resources/js/settings_modal.js
@@ -11,13 +11,17 @@ const network_password_input = document.getElementById("network_password_input")
 const create_new_network_btn = document.getElementById("create_new_network_btn");
 const network_list_ul = document.getElementById("network_list_ul");
 
-const lang_select_div = document.getElementById("lang_select_div");
-const lang_select_btn = document.getElementById("lang_select_btn");
-const language_dropdown_div = document.getElementById("language_dropdown_div");
+const audio_control_div = document.getElementById("audio_control_div");
+const audio_control_btn = document.getElementById("audio_control_btn");
+const audio_control_io_div = document.getElementById("audio_control_io_div");
 
 const face_encoding_div = document.getElementById("face_encoding_div");
 const face_encoding_btn = document.getElementById("face_encoding_btn");
 const face_encoding_io_div = document.getElementById("face_encoding_io_div");
+
+const lang_select_div = document.getElementById("lang_select_div");
+const lang_select_btn = document.getElementById("lang_select_btn");
+const language_dropdown_div = document.getElementById("language_dropdown_div");
 
 
 
@@ -51,17 +55,19 @@ update_control_btn.addEventListener("click", function () {
 
         dark_deep_background_div.classList.toggle("focused");
         dark_overlay_active = true;
-        toggle_elements([wifi_control_div, face_encoding_div, lang_select_div]);
+        toggle_elements([wifi_control_div, audio_control_div, face_encoding_div, lang_select_div]);
         update_control_div.classList.toggle("col");
         update_control_div.classList.toggle("focused");
+        update_control_div.classList.toggle("higher");
         update_control_io_div.classList.toggle("focused");
 
     } else {
         dark_deep_background_div.classList.toggle("focused");
         dark_overlay_active = false;
-        toggle_elements([wifi_control_div, face_encoding_div, lang_select_div]);
+        toggle_elements([wifi_control_div, audio_control_div, face_encoding_div, lang_select_div]);
         update_control_div.classList.toggle("col");
         update_control_div.classList.toggle("focused");
+        update_control_div.classList.toggle("higher");
         update_control_io_div.classList.toggle("focused");
 
         update_control_btn_click_count = 0;
@@ -106,9 +112,10 @@ wifi_connections_btn.addEventListener("click", function () {
                     dark_deep_background_div.classList.toggle("focused");
                     dark_overlay_active = true;
 
-                    toggle_elements([update_control_div, face_encoding_div, lang_select_div]);
+                    toggle_elements([update_control_div, audio_control_div, face_encoding_div, lang_select_div]);
                     wifi_control_div.classList.toggle("col");
                     wifi_control_div.classList.toggle("focused");
+                    wifi_control_div.classList.toggle("higher");
                     wifi_control_io_div.classList.toggle("focused");
 
                 }
@@ -125,6 +132,7 @@ wifi_connections_btn.addEventListener("click", function () {
         toggle_elements([update_control_div, face_encoding_div, lang_select_div]);
         wifi_control_div.classList.toggle("col");
         wifi_control_div.classList.toggle("focused");
+        wifi_control_div.classList.toggle("higher");
         wifi_control_io_div.classList.toggle("focused");
         wifi_connections_btn_click_count = 0;
     }
@@ -161,25 +169,36 @@ create_new_network_btn.addEventListener("click", function () {
     console.log(admin_id)
 });
 
+audio_control_btn.addEventListener("click", function () {
+
+    dark_overlay_active = !dark_deep_background_div.classList.contains("focused");
+    dark_deep_background_div.classList.toggle("focused");
+    toggle_elements([update_control_div, wifi_control_div, face_encoding_div, lang_select_div]);
+    audio_control_div.classList.toggle("col");
+    audio_control_div.classList.toggle("focused");
+    audio_control_div.classList.toggle("higher");
+    audio_control_io_div.classList.toggle("focused");
+});
+
+face_encoding_btn.addEventListener("click", function () {
+
+    dark_overlay_active = !dark_deep_background_div.classList.contains("focused");
+    dark_deep_background_div.classList.toggle("focused");
+    toggle_elements([update_control_div, wifi_control_div, audio_control_div, lang_select_div]);
+    face_encoding_div.classList.toggle("col");
+    face_encoding_div.classList.toggle("focused");
+    face_encoding_div.classList.toggle("higher");
+    face_encoding_io_div.classList.toggle("focused");
+});
 
 lang_select_btn.addEventListener("click", function () {
 
     dark_overlay_active = !dark_deep_background_div.classList.contains("focused");
     dark_deep_background_div.classList.toggle("focused");
 
-    toggle_elements([update_control_div, wifi_control_div, face_encoding_div]);
+    toggle_elements([update_control_div, wifi_control_div, audio_control_div, face_encoding_div]);
     lang_select_div.classList.toggle("col");
     lang_select_div.classList.toggle("focused");
+    lang_select_div.classList.toggle("higher");
     language_dropdown_div.classList.toggle("focused");
-});
-
-
-face_encoding_btn.addEventListener("click", function () {
-
-    dark_overlay_active = !dark_deep_background_div.classList.contains("focused");
-    dark_deep_background_div.classList.toggle("focused");
-    toggle_elements([update_control_div, wifi_control_div, lang_select_div]);
-    face_encoding_div.classList.toggle("col");
-    face_encoding_div.classList.toggle("focused");
-    face_encoding_io_div.classList.toggle("focused");
 });
