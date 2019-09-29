96b0c979ae436d7c42a48f09bb142cd94661c1bf
baybars <cem.baybars@gmail.com>
2019-09-29 19:06:08 +0300

    :sparkles: Add Fan and Cooler classes for stabilizing the cpu temperature to stand module.

--- t_system/stand/__init__.py
+++ t_system/stand/__init__.py
@@ -91,10 +91,10 @@ class Button:
 
 
 class Led:
-    """Class to define input buttons of T_System's stand interface.
+    """Class to define sign LEDs of T_System's stand interface.
 
-        This class provides necessary initiations and a function named :func:`t_system.stand.Button.__set_press_state`
-        for holding press event info.
+        This class provides necessary initiations and a function named :func:`t_system.stand.Led.on`
+        for activating the LED and a function named :func:`t_system.stand.Led.off` for deactivating the LED.
 
     """
 
@@ -125,6 +125,180 @@ class Led:
         GPIO.cleanup(self.gpio_pin)
 
 
+class Fan:
+    """Class to define cooler fans of T_System's stand interface.
+
+        This class provides necessary initiations and a function named :func:`t_system.stand.Fan.change_speed`
+        for changing revolute speed by incoming percent value.
+
+    """
+
+    def __init__(self, gpio_pin):
+        """Initialization method of :class:`t_system.stand.Fan` class.
+
+        Args:
+            gpio_pin (int):       	    GPIO pin to use for Fan.
+        """
+
+        self.gpio_pin = gpio_pin
+
+        self.gpio_pin = gpio_pin
+
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.gpio_pin, GPIO.OUT)
+        GPIO.setwarnings(False)
+
+        self.servo = GPIO.PWM(gpio_pin, 50)  # GPIO pin for PWM with 50Hz
+
+        self.max_duty_cy = 12.5
+        self.min_duty_cy = 2.5
+        self.current_duty_cy = None
+
+    def start(self, init_percent):
+        """Method to start of the fan initially.
+
+        Args:
+            init_percent (float):     Initialization percentage speed value for the fan.
+        """
+        init_duty_cy = self.__percent_to_duty_cy(init_percent)
+
+        self.servo.start(init_duty_cy)
+        self.current_duty_cy = init_duty_cy
+
+    def change_speed(self, speed_percent):
+        """Method to change of fan speed.
+
+        Args:
+            speed_percent (float):     Percentage speed value for the fan to set to it.
+        """
+
+        if speed_percent == 0:
+            self.__sleep()
+
+        self.__change_duty_cycle(self.__percent_to_duty_cy(speed_percent))
+
+    def __change_duty_cycle(self, duty_cycle):
+        """Method to handle changing duty-cycle of fan by given duty cycle value.
+
+        Args:
+            duty_cycle:               Cycle parameter of PWM signal.
+        """
+
+        self.servo.ChangeDutyCycle(duty_cycle)
+
+    def __percent_to_duty_cy(self, percent):
+        """Method to convert percentage to the duty cycle.
+
+        Args:
+            percent (float):     The speed percentage of the cooler fan.
+        """
+
+        return (percent / 100) * (self.max_duty_cy - self.min_duty_cy) + self.min_duty_cy
+
+    def __sleep(self):
+        """Method to provide stop the sending signal to fan's GPIO pin until coming new command.
+        """
+
+        self.servo.ChangeDutyCycle(0)  # If duty cycle has been set 0 (zero), no signal sending to GPIO pin.
+
+    def stop(self):
+        """Method to provide stop the GPIO.PWM service that is reserved the fan.
+        """
+
+        self.servo.stop()
+
+    def gpio_cleanup(self):
+        """Method to provide clean the GPIO pin that is reserved the fan.
+        """
+
+        GPIO.cleanup(self.gpio_pin)
+
+
+class Cooler:
+    """Class to define cooler of T_System's stand interface.
+
+        This class provides necessary initiations and a function named :func:`t_system.stand.Cooler.__stabilize_temperature`
+        for holding press event info.
+
+    """
+
+    def __init__(self, fan_pins):
+        """Initialization method of :class:`t_system.stand.Button` class.
+
+        Args:
+            fan_pins (list):       	    GPIO pins to use for fans.
+        """
+
+        self.fans = []
+
+        for pin in fan_pins:
+            fan = Fan(pin)
+            fan.start(25)
+
+            self.fans.append(fan)
+
+        self.min_temperature = 45
+        self.max_temperature = 80
+
+        self.stop_thread = False
+        self.cooling_thread = threading.Thread(target=self.__stabilize_temperature, args=(lambda: self.stop_thread,))
+
+    def start(self):
+        """Method to start of the asynchronous cooling process of cooler.
+        """
+
+        if not self.cooling_thread.is_alive():
+            self.cooling_thread = threading.Thread(target=self.__stabilize_temperature, args=(lambda: self.stop_thread,))
+            self.cooling_thread.start()
+
+    def __stabilize_temperature(self, stop_thread):
+        """Method to provide asynchronous temperature control point for running fans by the temperature.
+
+        Args:
+                stop_thread:       	    Stop flag of the tread about terminating it outside of the function's loop.
+        """
+
+        from gpiozero import CPUTemperature
+        cpu = CPUTemperature()
+
+        while True:
+            temperature = float(cpu.temperature)
+
+            for fan in self.fans:
+                if temperature <= 45:
+                    fan.change_speed(0)
+                elif temperature <= 75:
+                    fan.change_speed(100)
+                else:
+                    fan.change_speed(self.__temperature_to_percent(temperature))
+
+            if stop_thread():
+                break
+
+            time.sleep(1.5)
+
+    def __temperature_to_percent(self, temperature):
+        """Method to convert temperature to percentage.
+
+        Args:
+            temperature (float):     The temperature value of the cpu. Taken as celsius.
+        """
+
+        return ((temperature - self.min_temperature) / (self.max_temperature - self.min_temperature)) * 100
+
+    def stop(self):
+        """Method to provide stop the GPIO.PWM service that is reserved the cooler fans.
+        """
+
+        if self.cooling_thread.is_alive():
+            self.stop_thread = True
+            self.cooling_thread.join()
+
+        for fan in self.fans:
+            fan.stop()
+            fan.gpio_cleanup()
+
+
 class Stand:
     """Class to define tracking system's stand interface.
 
@@ -152,11 +326,16 @@ class Stand:
         self.red_led = Led(args["stand_gpios"][0])
         self.green_led = Led(args["stand_gpios"][1])
 
+        self.cooler = Cooler([args["stand_gpios"][2]])
+
         self.stop_thread = False
 
     def run(self):
         """The top-level method to managing members of stand interface.
         """
+
+        self.cooler.start()
+
         is_connected_to_network = self.network_connector.is_connected_to_network()
 
         if not is_connected_to_network:
@@ -233,3 +412,5 @@ class Stand:
 
         self.red_led.gpio_cleanup()
         self.green_led.gpio_cleanup()
+
+        self.cooler.stop()
