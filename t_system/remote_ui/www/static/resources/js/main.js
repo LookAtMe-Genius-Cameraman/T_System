0e07e015761e2448473c747c3a76877d3ba637f7
baybars <cem.baybars@gmail.com>
2019-10-02 14:58:43 +0300

    :art: rename the method 'connectdlg' to 'set_language_processes' and change the its functionality to creating language dropdown list and setting UI language initially.

--- t_system/remote_ui/www/static/resources/js/main.js
+++ t_system/remote_ui/www/static/resources/js/main.js
@@ -33,30 +33,6 @@ const controlling_template_container = document.getElementById("controlling_temp
 const options_template_container = document.getElementById("options_template_container");
 const prepare_template_container = document.getElementById("prepare_template_container");
 
-
-/**
- * Method to create drop-down language selection menu.
- */
-function build_language_menu() {
-    let content = "";
-    for (let lang_i = 0; lang_i < language_list.length; lang_i++) {
-        content += "<a href='#' onclick=\"translate_text('";
-        content += language_list[lang_i][0];
-        content += "'); update_ui_text();\"><span >";
-        content += language_list[lang_i][1];
-        content += "</span><span class=\"clearfix\"></span></a>";
-        if (language_list[lang_i][0] === language) {
-            document.getElementById("translate_menu").innerHTML = language_list[lang_i][1];
-        }
-    }
-    document.getElementById("lang_menu").innerHTML = content;
-}
-
-
-function update_ui_text() {
-    build_HTML_setting_list(current_setting_filter);
-}
-
 function toggle_elements(elements) {
     for (let i = 0; i < elements.length; i++) {
         elements[i].classList.toggle("hidden_element")
@@ -141,12 +117,21 @@ function resize_image(src, max_width, max_height, dest_element) {
 }
 
 
+function set_language_processes() {
+    let value = get_localdata('language');
+    if (String(value).length === 0 || String(value) === "null") value = "en";
+    build_language_menu();
+    translate_text(value, lang_select_dd_btn);
+}
+
+
 let swiper;
+
 $(document).ready(function () {
     initial_loading_div.classList.add("inactive");
     loading_animation_div.classList.remove("lds-dual-ring");
 
-    connectdlg();
+    set_language_processes();
 
     swiper = new Swiper('.swiper-container', {
         effect: 'cube',
