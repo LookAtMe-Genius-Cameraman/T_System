2c37539f14a493e133ee83feee8914d26de12c18
baybars <cem.baybars@gmail.com>
2019-10-08 15:47:38 +0300

    :fire: Remove the unused key from English language support.

--- t_system/remote_ui/www/static/resources/js/language/en.js
+++ t_system/remote_ui/www/static/resources/js/language/en.js
@@ -1,9 +1,2 @@
 //english
-const englishtrans = {
-    // "STA": "Client Station",
-    // "AP": "Access Point",
-    // "Basic_Dashboard": "Basic",
-    // "Dashboard": "Advanced",
-
-    // Dil desteği ayarlanacak.
-};
+const englishtrans = {};
