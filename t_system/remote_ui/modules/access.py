baf0741cfee01c809271114dd712db951573687b
baybars <cem.baybars@gmail.com>
2019-10-10 15:58:14 +0300

    :newspaper: Add access module to Remote UI modules for officiating accession jobs to T_System from remote devices.

---
+++ t_system/remote_ui/modules/access.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+.. module:: access
+    :platform: Unix
+    :synopsis: the top-level submodule of T_System's remote_ui that contains the functions for accessing t_system from remote applications.
+
+.. moduleauthor:: Cem Baybars GÜÇLÜ <cem.baybars@gmail.com>
+"""
+
+from t_system import identifier
+
+
+def check_private_id(private_id):
+    """Method to verify incoming private id of T_System.
+
+    Args:
+        private_id (str):              Private id of identifier of T_System.
+    """
+
+    if private_id == identifier.private_id:
+        return True
+    return False
+
+
+def get_private_id_by(data):
+    """Method to get private id of T_System if given data contains the public id.
+
+    Args:
+        data (dict):                    Access data structure.
+    """
+
+    if data["id"] == identifier.public_id:
+        return identifier.private_id
+    return False
