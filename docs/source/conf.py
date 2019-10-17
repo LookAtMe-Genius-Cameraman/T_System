42278e3fe092eaa87cd339636a64623cf3bec073
baybars <cem.baybars@gmail.com>
2019-10-17 16:22:21 +0300

    :bookmark: Bump the version t_system 0.9-beta.63 -> 0.9-beta.64 remote_ui 0.9.93 -> 0.9.94

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,7 @@ author = 'Cem Baybars GÜÇLÜ'
 # The short X.Y version
 version = '0.9'
 # The full version, including alpha/beta/rc tags
-release = '0.9-beta.63'
+release = '0.9-beta.64'
 
 
 # -- General configuration ---------------------------------------------------
