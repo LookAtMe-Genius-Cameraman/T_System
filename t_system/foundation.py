82db3b1b1874d3b2c64d483c7f784f9710e7d13d
baybars <cem.baybars@gmail.com>
2019-11-04 09:57:31 +0300

    :tractor: Change the foundation module's root priveleges logic.

--- t_system/foundation.py
+++ t_system/foundation.py
@@ -12,83 +12,84 @@
 import psutil
 from gpiozero import CPUTemperature
 
-from t_system.administration import is_admin
-
 from t_system import log_manager
 
 logger = log_manager.get_logger(__name__, "DEBUG")
 
 
-def get_disk_usage(admin_id):
+def get_disk_usage(is_admin):
     """Method to provide getting system's disk usage.
 
     Args:
-        admin_id:   	     ID of the administration authentication.
+        is_admin (bool):     Root privileges flag.
 
     Returns:
         dict:  Response.
     """
     usage = psutil.disk_usage('/')
 
-    if is_admin(admin_id):
+    if is_admin:
         return {"disk_usage_percent": round(usage.percent, 2), "free_disk_space": round(usage.free / 10 ** 9, 2)}
     else:
         return {"disk_usage_percent": round(usage.percent * 1.1, 2), "free_disk_space": round(usage.free / 10 ** 9 * 0.9, 2)}
 
 
-def get_cpu_usage(admin_id):
+def get_cpu_usage(is_admin):
     """Method to provide getting system's cpu usage.
 
     Args:
-        admin_id:   	     ID of the administration authentication.
+        is_admin (bool):     Root privileges flag.
 
     Returns:
             dict:  Response.
     """
-    if is_admin(admin_id):
+    if is_admin:
         return {"cpu_usage_percent": psutil.cpu_percent()}
     else:
         return {"cpu_usage_percent": None}
 
 
-def get_ram_usage(admin_id):
+def get_ram_usage(is_admin):
     """Method to provide getting system's ram usage.
     Args:
-        admin_id:   	     ID of the administration authentication.
+        is_admin (bool):     Root privileges flag.
 
     Returns:
             dict:  Response.
     """
-    if is_admin(admin_id):
+    if is_admin:
         return {"ram_usage_percent": psutil.virtual_memory()[2]}
     else:
         return {"ram_usage_percent": None}
 
 
-def get_cpu_temperature(admin_id):
+def get_cpu_temperature(is_admin):
     """Method to provide getting temperature of system's components.
 
     Args:
-        admin_id:   	     ID of the administration authentication.
+        is_admin (bool):     Root privileges flag.
 
     Returns:
             dict:  Response.
     """
 
-    if is_admin(admin_id):
+    if is_admin:
         cpu = CPUTemperature()
         return {"cpu_temperature": cpu.temperature}
     else:
         return {"cpu_temperature": None}
 
 
-def get_versions(admin_id):
+def get_versions(is_admin):
     """Method to provide getting versions of `t_system`, `remote_ui` and `stand`.
+
+    Args:
+        is_admin (bool):     Root privileges flag.
 
     Returns:
             dict:  Response.
     """
-    if is_admin(admin_id):
+    if is_admin:
         from t_system import __version__ as t_system_version
         from t_system.stand import __version__ as stand_version
         from t_system.remote_ui import __version__ as remote_ui_version
