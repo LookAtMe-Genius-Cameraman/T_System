723b58853c7dd0a19282adc29a4eab55935606cc
baybars <cem.baybars@gmail.com>
2019-12-13 15:58:48 +0300

    :sparkles: Add live_stream API of Remote UI for handling OnlineStreamer of T_System's Vision.

---
+++ t_system/remote_ui/api/live_stream.py
@@ -0,0 +1,159 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+.. module:: live_stream
+    :platform: Unix
+    :synopsis: the top-level submodule of T_System's remote_ui that contains the API for T_System Vision's live Stream ability to several Websites.
+
+.. moduleauthor:: Cem Baybars GÜÇLÜ <cem.baybars@gmail.com>
+"""
+
+from flask import Blueprint, request
+from flask_restful import Api, Resource
+from schema import SchemaError
+
+from t_system.remote_ui.modules.live_stream import switch_live_stream, set_website_usage_status, set_stream_id_u_status, upsert_website, get_website, get_websites, delete_website, create_stream_id, update_stream_id, delete_stream_id
+from t_system.remote_ui.api.data_schema import L_STREAM_SCHEMA, L_S_WEBSITE_SCHEMA
+
+from t_system import log_manager
+
+logger = log_manager.get_logger(__name__, "DEBUG")
+
+api_bp = Blueprint('live_stream_api', __name__)
+api = Api(api_bp)
+
+
+class LiveStreamApi(Resource):
+    """Class to define an API of the Live Stream ability of T_System.
+
+        This class provides necessary initiations and functions named;
+         :func:`t_system.remote_ui.api.live_stream.LiveStreamApi.get`for the provide get live_stream data from database,
+         :func:`t_system.remote_ui.api.live_stream.LiveStreamApi.post` for provide creating new Websites and Stream IDs,
+         :func:`t_system.remote_uia.api.live_stream.LiveStreamApi.put` for provide updating the Websites and Stream IDs,
+         :func:`t_system.remote_ui.api.live_stream.LiveStreamApi.delete` for provide deleting the Websites and Stream IDs.
+    """
+
+    def __init__(self):
+        """Initialization method of :class:`t_system.remote_ui.api.live_stream.LiveStreamApi` class.
+        """
+
+    def get(self):
+        """The API method to GET request for flask.
+        """
+
+        website_id = request.args.get('id', None)
+        admin_id = request.args.get('admin_id', None)
+        root = request.args.get('root', None)
+
+        if website_id:
+            website = get_website(admin_id, root, website_id)
+            return {'status': 'OK', 'data': website}
+
+        websites = get_websites(admin_id, root)
+
+        return {'status': 'OK', 'data': websites}
+
+    def post(self):
+        """The API method to POST request for flask.
+        """
+        website_id = request.args.get('id', None)
+        admin_id = request.args.get('admin_id', None)
+        root = request.args.get('root', None)
+
+        if not website_id:
+            try:
+                data = L_S_WEBSITE_SCHEMA.validate(request.json)
+            except SchemaError as e:
+                return {'status': 'ERROR', 'message': e.code}
+
+            result = upsert_website(admin_id, root, data)
+
+            return {'status': 'OK' if result else 'ERROR'}
+
+        try:
+            data = L_STREAM_SCHEMA.validate(request.json)
+        except SchemaError as e:
+            return {'status': 'ERROR', 'message': e.code}
+
+        logger.debug("stream ID creation starting...")
+
+        result = create_stream_id(admin_id, root, website_id, data)
+
+        return {'status': 'OK' if result else 'ERROR'}
+
+    def put(self):
+        """The API method to PUT request for flask.
+        """
+        website_id = request.args.get('id')
+        account_name = request.args.get('account_name', None)
+        admin_id = request.args.get('admin_id', None)
+        root = request.args.get('root', None)
+
+        if not website_id:
+            return {'status': 'ERROR', 'message': '\'id\' parameter is missing'}
+
+        if account_name:
+            try:
+                data = L_STREAM_SCHEMA.validate(request.json)
+            except SchemaError as e:
+                return {'status': 'ERROR', 'message': e.code}
+
+            result = update_stream_id(admin_id, root, website_id, data)
+        else:
+            try:
+                data = L_S_WEBSITE_SCHEMA.validate(request.json)
+            except SchemaError as e:
+                return {'status': 'ERROR', 'message': e.code}
+
+            result = upsert_website(admin_id, root, data, force_insert=True)
+
+        return {'status': 'OK' if result else 'ERROR'}
+
+    def patch(self):
+        """The API method to PATCH request for flask.
+        """
+        cause = request.args.get('cause')
+        in_use = request.args.get('in_use')
+        website_id = request.args.get('id', None)
+        account_name = request.args.get('account_name', None)
+        admin_id = request.args.get('admin_id', None)
+
+        result = False
+
+        if not cause:
+            return {'status': 'ERROR', 'message': '\'cause\' parameter is missing'}
+
+        if not in_use:
+            return {'status': 'ERROR', 'message': '\'cause\' parameter is missing'}
+
+        if cause == "website":
+            result = set_website_usage_status(admin_id, website_id, in_use)
+        elif cause == "stream_id":
+            result = set_stream_id_u_status(admin_id, website_id, account_name, in_use)
+        elif cause == "live":
+            result = switch_live_stream(admin_id, in_use)
+
+        return {'status': 'OK' if result else 'ERROR'}
+
+    def delete(self):
+        """The API method to DELETE request for flask.
+        """
+
+        website_id = request.args.get('id')
+        account_name = request.args.get('account_name', None)
+        admin_id = request.args.get('admin_id', None)
+        root = request.args.get('root', None)
+
+        if not website_id:
+            return {'status': 'ERROR', 'message': '\'id\' parameter is missing'}
+
+        if account_name:
+            result = delete_stream_id(admin_id, root, website_id, account_name)
+        else:
+            result = delete_website(admin_id, root, website_id)
+
+        return {'status': 'OK' if result else 'ERROR'}
+
+
+api.add_resource(LiveStreamApi, '/api/live_stream')
---
+++ t_system/remote_ui/modules/live_stream.py
@@ -0,0 +1,213 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+.. module:: live_stream
+    :platform: Unix
+    :synopsis: the top-level submodule of T_System's remote_ui that contains the functions for Live Stream ability of T_System Vision.
+
+.. moduleauthor:: Cem Baybars GÜÇLÜ <cem.baybars@gmail.com>
+"""
+
+from t_system.administration import is_admin
+
+from t_system import seer
+
+from t_system import log_manager
+
+logger = log_manager.get_logger(__name__, "DEBUG")
+
+
+def switch_live_stream(admin_id, in_use):
+    """Method to start / stop live streaming.
+
+    Args:
+            admin_id (str):                 Admin privileges flag.
+            in_use (bool):                  usage status flag.
+    """
+
+    try:
+        if in_use:
+            seer.online_streamer.go_live()
+        else:
+            seer.online_streamer.stop_live()
+
+        return True
+    except Exception as e:
+        logger.error(e)
+        return False
+
+
+def set_stream_id_u_status(admin_id, website_id, account_name, in_use):
+    """Method to set usage status of live stream's a stream ID of a website.
+
+    Args:
+            admin_id (str):                 Admin privileges flag.
+            website_id (str):               The id of the position.
+            account_name (str):             Name of the website's account.
+            in_use (bool):                  usage status flag.
+    """
+
+    return seer.online_streamer.activate_website_stream(website_id, account_name)
+
+
+def set_website_usage_status(admin_id, website_id, in_use):
+    """Method to set usage status of live stream's websites.
+
+    Args:
+            admin_id (str):                 Admin privileges flag.
+            website_id (str):               The id of the position.
+            in_use (bool):                  usage status flag.
+    """
+
+    return seer.online_streamer.set_website_usage_stat(website_id, in_use)
+
+
+def create_stream_id(admin_id, root, website_id, data):
+    """Method to create new stream ID for given website.
+
+    Args:
+            admin_id (str):                 Admin privileges flag.
+            root (str):                     Root privileges activation flag.
+            website_id (str):               The id of the position.
+            data (dict):                    Stream ID data structure.
+    """
+
+    try:
+
+        result = seer.online_streamer.set_website_stream(website_id, data)
+
+    except Exception:
+        result = False
+
+    return result
+
+
+def update_stream_id(admin_id, root, website_id, data):
+    """Method to update the Stream ID of given website.
+
+    Args:
+            admin_id (str):                 Root privileges flag.
+            root (str):                     Root privileges activation flag.
+            website_id (str):               The id of the website.
+            data (dict):                    Stream ID data structure.
+    """
+
+    return seer.online_streamer.set_website_stream(website_id, data)
+
+
+def delete_stream_id(admin_id, root, website_id, account_name):
+    """Method to remove existing website with given id.
+
+    Args:
+            admin_id (str):                 Root privileges flag.
+            root (str):                     Root privileges activation flag.
+            website_id (str):               The id of the website.
+            account_name (str):             Name of the website's account.
+    """
+
+    return seer.online_setreamer.remove_website_stream(website_id, account_name)
+
+
+def upsert_website(admin_id, root, data, force_insert=False):
+    """Method to update and insert new website to live streaming.
+
+    Args:
+            admin_id (str):                 Admin privileges flag.
+            root (str):                     Root privileges activation flag.
+            data (dict):                    website data structure.
+            force_insert (bool):            Force insert Flag for updating the website information.
+    """
+
+    try:
+        if not is_admin(admin_id):
+            root = False
+        else:
+            root = root in ["true", "True"]
+
+        if root:
+            result = seer.online_streamer.add_website(data["name"], data["url"], data["server"], force_insert=force_insert)
+        else:
+            result = False
+
+    except Exception:
+        result = False
+
+    return result
+
+
+def get_websites(admin_id, root):
+    """Method to return existing websites.
+
+    Args:
+            admin_id (str):                 Root privileges flag.
+            root (str):                     Root privileges activation flag.
+    """
+    result = []
+    try:
+        if not is_admin(admin_id):
+            root = False
+        else:
+            root = root in ["true", "True"]
+
+        websites = seer.online_streamer.get_websites()
+
+        if websites:
+            for website in websites:
+                result.append({"id": website.id, "name": website.name, "url": website.url, "server": website.server, "to_be_used": website.to_be_used, "stream_ids": website.stream_ids})
+
+    except Exception as e:
+        logger.error(e)
+        result = []
+
+    return result
+
+
+def get_website(admin_id, root, website_id):
+    """Method to return existing website with given id.
+
+    Args:
+            admin_id (str):                 Root privileges flag.
+            root (str):                     Root privileges activation flag.
+            website_id (str):               The id of the position.
+    """
+    result = []
+
+    try:
+        if not is_admin(admin_id):
+            root = False
+        else:
+            root = root in ["true", "True"]
+
+        websites = seer.online_streamer.get_websites(w_ids=[website_id])
+
+        if websites:
+            for website in websites:
+                result.append({"id": website.id, "name": website.name, "url": website.url, "server": website.server, "to_be_used": website.to_be_used, "stream_ids": website.stream_ids})
+
+    except Exception as e:
+        logger.error(e)
+        result = []
+
+    return result
+
+
+def delete_website(admin_id, root, website_id):
+    """Method to remove existing website with given id.
+
+    Args:
+            admin_id (str):                 Root privileges flag.
+            root (str):                     Root privileges activation flag.
+            website_id (str):               The id of the website.
+    """
+    if not is_admin(admin_id):
+        root = False
+    else:
+        root = root in ["true", "True"]
+
+    if root:
+        result = seer.online_setreamer.remove_websites([website_id])
+    else:
+        result = True
+
+    return result
